a835c0bb1870a2ad4c1679e55211829155ac7ab9
kah3007 <anhkietncong@gmail.com>
2023-09-07 19:50:40 +0700

    Lab2-1,2-2

---
+++ Lab/ch02_ex1/display_discount.php
@@ -0,0 +1,39 @@
+<?php
+    $product_description = filter_input(INPUT_POST, 'product_description');
+    $list_price = filter_input(INPUT_POST,'list_price');
+    $discount_percent = filter_input(INPUT_POST, 'discount_percent');
+    $discount_amount = $list_price * $discount_percent / 100;
+    $discount_price = $list_price - $discount_amount;
+    
+    $list_price = '$'.number_format($list_price,2);
+    $discount_percent = $discount_percent.'%';
+    $discount_amount = '$'.number_format($discount_amount,2);
+    $discount_price = '$'.number_format($discount_price,2);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <main>
+        <h1>This page is under construction</h1>
+
+        <label>Product Description:</label>
+        <span><?php echo htmlspecialchars($product_description); ?></span><br>
+
+        <label>List Price:</label>
+        <span><?php echo htmlspecialchars($list_price); ?></span><br>
+
+        <label>Standard Discount:</label>
+        <span><?php echo htmlspecialchars($discount_percent); ?></span><br>
+
+        <label>Discount Amount:</label>
+        <span><?php echo $discount_amount; ?></span><br>
+
+        <label>Discount Price:</label>
+        <span><?php echo $discount_price; ?></span><br>
+    </main>
+</body>
+</html>
