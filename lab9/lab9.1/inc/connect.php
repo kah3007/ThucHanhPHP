c1162aa2a95daaefab41bb2db0b0783fdf8bb1da
kah3007 <144111252+kah3007@users.noreply.github.com>
2023-11-19 09:46:30 +0700

    Delete lab9 directory

--- lab9/lab9.1/inc/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server=$_SERVER['SERVER_NAME'];
-    $user = 'root';
-    $pass="";
-    $db="test";
-    $con=mysqli_connect($server,$user,$pass,$db);
-    mysqli_query($con,"set names 'utf8'");
-?>
